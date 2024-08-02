ab0a4a2a6e8c86edfa225e4b5d762ce19de8d6b3
kwamenaduker <102928723+kwamenaduker@users.noreply.github.com>
2024-08-02 02:28:26 +0000

    TrafficLight.java

--- DSA Project/TrafficLight.java
+++ DSA Project/TrafficLight.java
@@ -1,10 +1,11 @@
 public class TrafficLight {
-
     private String direction;
     private String color;
     private int redDuration;
     private int yellowDuration;
     private int greenDuration;
+
+    // Constructor and other methods
 
     public TrafficLight (String direction, int redDuration, int yellowDuration, int greenDuration){
         this.direction = direction;
@@ -13,46 +14,51 @@ public class TrafficLight {
         this.yellowDuration = yellowDuration;
         this.greenDuration = greenDuration;
     }
-
+    
     public String getDirection() {
         return direction;
     }
 
-    public String getColor(){
-        return color;
+    public void setDirection(String direction) {
+        this.direction = direction;
     }
 
     public void setColor(String color) {
         this.color = color;
     }
 
-    public int getRedDuration() {
-        return redDuration;
+    public String getColor() {
+        return color;
     }
 
-    public void setDuration (redDuration) {
-        this.redDuration = redDuration;
+    public int getRedDuration() {
+        return redDuration;
     }
 
     public int getYellowDuration() {
         return yellowDuration;
     }
 
-    public void setYellowDuration (yellowDuration) {
-        this.yellowDuration = yellowDuration;
-    }
-
     public int getGreenDuration() {
         return greenDuration;
     }
 
-    public void setGreenDuration (greenDuration) {
-        this.greenDuration = greenDuration;
+    public void setDuration(String color, int duration) {
+        switch (color.toLowerCase()) {
+            case "red":
+                this.redDuration = duration;
+                break;
+            case "yellow":
+                this.yellowDuration = duration;
+                break;
+            case "green":
+                this.greenDuration = duration;
+                break;
+        }
     }
 
     public void changeLight(String newColor) {
         setColor(newColor);
-        System.out.println(direction + " traffic light color has changed to " + newColor)
+        System.out.println(direction + " traffic light color has changed to " + newColor);
     }
-
 }

5a4b86deee372c495a68d7b007acf73c2f098d6e
kwamenaduker <102928723+kwamenaduker@users.noreply.github.com>
2024-08-02 02:28:54 +0000

    TrafficLightManager.java

--- DSA Project/TrafficLightManager.java
+++ DSA Project/TrafficLightManager.java
@@ -1,47 +1,74 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TrafficLightManager {
-    private Map <String, TrafficLight> trafficLights;
+    private Map<String, TrafficLight> trafficLights;
+    private int redDuration;
+    private int yellowDuration;
+    private int greenDuration;
 
     public TrafficLightManager() {
-        trafficLights = new HashMap<>();
+        redDuration = 30;  // Default red light duration in seconds
+        yellowDuration = 5;  // Default yellow light duration in seconds
+        greenDuration = 30;  // Default green light duration in seconds
 
-        trafficLights.put("north", new TrafficLights("north", 30, 5, 30));
-        trafficLights.put("south", new TrafficLights("south", 30, 5, 30));
-        trafficLights.put("east", new TrafficLights("east", 30, 5, 30));
-        trafficLights.put("west", new TrafficLights("west", 30, 5, 30));
+        trafficLights = new HashMap<>();
+        trafficLights.put("north", new TrafficLight("north", redDuration, yellowDuration, greenDuration));
+        trafficLights.put("south", new TrafficLight("south", redDuration, yellowDuration, greenDuration));
+        trafficLights.put("east", new TrafficLight("east", redDuration, yellowDuration, greenDuration));
+        trafficLights.put("west", new TrafficLight("west", redDuration, yellowDuration, greenDuration));
     }
 
-    public void setLight (String direction, String color) {
-        if(trafficLights.containsKey(direction)){
-            trafficLights.get(direction).changeLight(color);
-        }
-        else{
+    public void setLight(String direction, String color) {
+        TrafficLight light = trafficLights.get(direction);
+        if (light != null) {
+            light.setColor(color);
+            System.out.println("Set " + direction + " light to " + color);
+        } else {
             System.out.println("Invalid direction: " + direction);
         }
     }
 
-    public void setLightTiming (String color, int duration) {
+    public String getColor(String direction) {
+        TrafficLight light = trafficLights.get(direction);
+        if (light != null) {
+            return light.getColor();
+        }
+        System.out.println("Invalid direction: " + direction);
+        return null;
+    }
+
+    public int getRedDuration() {
+        return redDuration;
+    }
+
+    public int getYellowDuration() {
+        return yellowDuration;
+    }
+
+    public int getGreenDuration() {
+        return greenDuration;
+    }
+
+    public void setDuration(String color, int duration) {
+        switch (color.toLowerCase()) {
+            case "red":
+                redDuration = duration;
+                break;
+            case "yellow":
+                yellowDuration = duration;
+                break;
+            case "green":
+                greenDuration = duration;
+                break;
+            default:
+                System.out.println("Invalid color: " + color);
+                return;
+        }
+
         for (TrafficLight light : trafficLights.values()) {
-            switch (color) {
-                case "red":
-                    light.setGreenDuration(duration);
-                    break;
-                case "yellow":
-                    light.setGreenDuration(duration);
-                    break;
-                case "green":
-                    light.setGreenDuration(duration);
-                    break;
-                default:
-                    System.out.println("Invalid color: " + color);    
-            } 
+            light.setDuration(color, duration);
         }
+        System.out.println("Set all lights " + color + " duration to " + duration + " seconds");
     }
-
-    public void printTrafficLights() {
-        for (TrafficLight light : trafficLights.values()){
-            System.out.println(light.getDirection() + "light is " + light,getColor());
-        }
-    }
-
-
 }
